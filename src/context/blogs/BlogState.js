ab0cdd4f2b6315bc3f729f8fd67920cdc1d28000
Sarthak Gupta <89603086+sarthak783891@users.noreply.github.com>
2022-12-15 20:01:57 +0530

    changed host from heroku to vercel

--- src/context/blogs/BlogState.js
+++ src/context/blogs/BlogState.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const BlogState = (props) => {
 
-  const host = "https://bloggerstopmern.herokuapp.com";
+  const host = "https://bloggerstop-backend.vercel.app";
   // let initialBlogs = [];
   let initialAlert = {
     state: false,
